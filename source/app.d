7355d2bc4a125b6b425b7b10a1973b5157fca032
timi95 <timi.ogunkeye@gmail.com>
2020-04-22 21:45:55 +0100

    First commit of working code for dsfml-agents

---
+++ source/app.d
@@ -0,0 +1,23 @@
+import std.stdio;
+
+import dsfml.graphics;
+import dsfml.system;
+
+
+void main()
+{
+	RenderWindow window =  new RenderWindow(VideoMode(500, 600), "DSFML");
+
+	while ( window.isOpen() ) {
+		Event event;
+		while ( window.pollEvent(event) ) {
+			if ( event.type == Event.EventType.Closed ) { 
+				window.close();
+			}
+		}
+
+		window.clear( Color(255, 50, 40) );
+		window.display();
+	}
+	writeln("Edit source/app.d to start your project.");
+}
