162d2b989f43fb327334f63b527f4bc8d98f8c52
timi95 <timi.ogunkeye@gmail.com>
2020-04-25 23:39:52 +0100

    drew a red circle
    Signed-off-by: timi95 <timi.ogunkeye@gmail.com>

--- source/app.d
+++ source/app.d
@@ -8,6 +8,23 @@ void main()
 {
 	RenderWindow window =  new RenderWindow(VideoMode(500, 600), "DSFML");
 
+    // Create a graphical text to display
+    auto font = new Font();
+    if(!font.loadFromFile("Organo.ttf"))
+        return;
+    auto text = new Text("Hello DSFML", font, 50);
+	
+
+	Vector2!float newPosition;
+	newPosition.x = 25.0;
+	newPosition.y = 25.0;
+	auto circle_color = new Color();
+	auto circle = new CircleShape();
+	circle.radius(10.0);
+	circle.fillColor(circle_color.Red);
+	circle.position(newPosition);
+
+
 	while ( window.isOpen() ) {
 		Event event;
 		while ( window.pollEvent(event) ) {
@@ -16,9 +33,21 @@ void main()
 			}
 		}
 
-		window.clear( Color(255, 50, 40) );
+		// Clear screen
+		window.clear();
+
+		// Draw the string
+		window.draw(text);
+
+		// Draw the circle
+		window.draw(circle);
+
+		// Update the window
 		window.display();
 	}
 	writeln("Edit source/app.d to start your project.");
 }
 //
+public 	class Agent () {
+
+}
